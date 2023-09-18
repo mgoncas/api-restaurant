6e247704a81abb237e97416d8798c9b448b7b697
Inma Caleron <inmacale@gmail.com>
2023-09-18 16:46:18 +0200

    subida de los repositorios creados

--- src/main/java/prueba/restaurant/apirestaurant/entities/Cliente.java
+++ src/main/java/prueba/restaurant/apirestaurant/entities/Cliente.java
@@ -12,8 +12,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="numero_mesa")
-    private Integer numMesa;
+
     @Column(name="telefono")
     private Integer telefono;
     @Column(name="direccion")

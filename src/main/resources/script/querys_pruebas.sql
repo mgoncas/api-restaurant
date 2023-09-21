790a8b185e7d5eb60e548aa74d070a515ab944eb
Inma Caleron <inmacale@gmail.com>
2023-09-21 13:39:27 +0200

    he añadido un spript

---
+++ src/main/resources/script/querys_pruebas.sql
@@ -0,0 +1,10 @@
+insert into cliente  (direccion, telefono) values ('madrid','689348280');
+insert into cliente  (direccion, telefono) values ('reales','789348280');
+insert into cliente  (direccion, telefono) values ('mad','619348280');
+
+SELECT * FROM TIPO_CARNE 
+
+insert into tipo_carne (nombre, precio)values('ternera','1.00');
+insert into tipo_carne (nombre, precio)values('angus','1.5');
+insert into tipo_carne (nombre, precio)values('buey','2.0');
+insert into tipo_carne (nombre, precio)values('retinto','2.5');
